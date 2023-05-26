edf1a7121db660b46bc162a55e37a92da8f31956
Erin Beate Bjorkeli <42803650+ebjorkeli@users.noreply.github.com>
2023-05-26 19:45:59 +0200

    Delete AC_multiheads.py

--- Models/AC_multiheads.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.layers import Input, Dense, Reshape, Conv1D, Conv1DTranspose, Flatten
-from tensorflow.keras.models import Model
-
-# Target tumor/healthy
-
-# Adversial
-
-class OutputHead(tf.keras.models.Model):
-    def __init__(self):
